50c338fe511cc781e3d164745c77530c8ce3dce4
Yan Kai <yankai06.23@ichat.sp.edu.sg>
2024-04-24 19:19:02 +0800

    median function added

--- Lab2.py
+++ Lab2.py
@@ -8,10 +8,13 @@ def get_user_input():
 
     for i in range(len(mylist)):
         mylist.append(float(mylist[0]))
+        #Append function add the new float no. to the back of the array
         mylist.pop(0)
+        #pop function would remove the index 0 of the array
+        #hence elements in the array is being changed from str to float one by one like a shift register
     return mylist
 
-['0','1','2']
+
 def sort_temperature():
     print("sort_temperature")
 
@@ -27,13 +30,26 @@ def calc_min_max_temp(value_list):
     max_value=max(value_list)
     return [min_value,max_value]
 
+def calc_median_temp(value_list):
+    asc_list = sorted(value_list)
+    check=len(asc_list)%2
+    if check==0:
+        index=len(asc_list)/2
+        index=int(index)
+        median=(asc_list[index]+asc_list[index-1])/2
+        return median
+    else:
+        index=len(asc_list)/2
+        index = int(index)
+        return asc_list[index]
 
 def main():
     print("ET0735 (DevOps for AIoT) - Lab 2 - Introduction to Python")
     display_main_menu()
     num_list= get_user_input()
-    print (calc_avg_temp(num_list))
-    print (calc_min_max_temp(num_list))
+    print ("Average: "+ str(calc_avg_temp(num_list)))
+    print ("[Min,Max]: " + str(calc_min_max_temp(num_list)))
+    print("Median: "+ str(calc_median_temp(num_list)))
 
 if __name__=="__main__":
     main()
