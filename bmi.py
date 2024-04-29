54c7547ccbd5ecc08e31936730a4edf847110d91
Yan Kai <yankai06.23@ichat.sp.edu.sg>
2024-04-29 15:07:19 +0800

    Overweight=1,normal=0,underweight=-1

--- bmi.py
+++ bmi.py
@@ -7,10 +7,13 @@ def calculate_bmi(height,weight):
     
     if bmi<18.5:
         print("User is underweight")
+        return -1
     elif 18.5<=bmi<=25.0:
         print("User is normal weight")
+        return 0
     else :
         print("User is overweight")
+        return 1
 
 
 calculate_bmi(weight=67, height=1.73)
